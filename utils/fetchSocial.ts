fe90c8d3049ebed8f5ec00e8b2d646d844c4aa9c
Mr-Biskit <dev@mrbiskit.xyz>
2023-01-02 18:37:11 +0100

    Changes for sanity

--- utils/fetchSocial.ts
+++ utils/fetchSocial.ts
@@ -1,7 +1,7 @@
 import { Social } from "../typings";
 
 export const fetchSocials = async () => {
-  const res = await fetch(`${process.env.NEXT_PUBLIC_URL}/api/getSocial`, {
+  const res = await fetch(`${process.env.NEXT_PUBLIC_BASE_URL}/api/getSocial`, {
     headers: {
       Accept: "application/json, text/plain, */*",
       "User-Agent": "*",
